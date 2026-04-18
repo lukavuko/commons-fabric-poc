9ad9c720f89044c34b122304944fb84d607c7e2b
Luka Vuk <luka.vuko@outlook.com>
2026-04-18 01:57:37 -0400

    Added prisma adapter and migrated the db

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,14 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
+    adapter,
     log: ['query', 'error', 'warn'],
   });
 
